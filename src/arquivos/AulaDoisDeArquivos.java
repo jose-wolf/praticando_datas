2ce140cb0937f8ed3a4bf23c12d55b14620312bf
José Wolf <wolf.jose89@gmail.com>
2024-08-14 23:37:27 -0300

    Adicionando aula de arquivos em Java

--- src/arquivos/AulaDoisDeArquivos.java
+++ src/arquivos/AulaDoisDeArquivos.java
@@ -1,2 +1,42 @@
-package arquivos;public class AulaDoisDeArquivos {
+package arquivos;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AulaDoisDeArquivos {
+
+    public static void main(String[] args) {
+
+        String[] lines = new String[] {
+                "Good morning",
+                "Good Afternoon",
+                "Segunda Aula de arquivos"
+        };
+
+        String path = "C:\\Users\\wolfj\\OneDrive\\Documentos\\comprimento2.txt";
+
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+            for(String line : lines){
+                bw.write(line);
+                bw.newLine();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        try(Scanner sc = new Scanner(new File(path))){
+
+            while(sc.hasNext()){
+                System.out.println(sc.nextLine());
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- src/arquivos/AulaUmDeArquivos.java
+++ src/arquivos/AulaUmDeArquivos.java
@@ -1,2 +1,26 @@
-package arquivos;public class AulaUmDeArquivos {
+package arquivos;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AulaUmDeArquivos {
+
+    public static void main(String[] args) {
+
+            String path = "C:\\Users\\wolfj\\OneDrive\\Documentos\\comprimento.txt";
+
+        try(Scanner sc = new Scanner(new File(path))){
+
+            while(sc.hasNext()){
+                System.out.println(sc.nextLine());
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
