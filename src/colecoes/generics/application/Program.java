f9873a953d6813e48c403952425d7fdb88aa28e1
José Wolf <wolf.jose89@gmail.com>
2024-09-02 22:40:04 -0300

    Conteúdo sobre generics

--- src/colecoes/generics/application/Program.java
+++ src/colecoes/generics/application/Program.java
@@ -1,2 +1,30 @@
-package colecoes.generics.application;public class Program {
+package colecoes.generics.application;
+
+import colecoes.generics.services.PrintService;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        PrintService<String> ps = new PrintService<>();
+
+        System.out.print("Quantos valores: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++){
+            String value = sc.next();
+            ps.addValue(value);
+        }
+
+        System.out.println("First: " + ps.first());
+
+        ps.print();
+
+        sc.close();
+    }
+
 }
